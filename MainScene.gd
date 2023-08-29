71e8b641163a6dd7bd2e03193f1d0bdf33fc3f85
mgrimsl <xmikegrim@gmail.com>
2023-08-28 23:06:48 -0400

    adding server pack

--- MainScene.gd
+++ MainScene.gd
@@ -17,18 +17,15 @@ func _on_host_pressed():
 	#cam.clear_current()
 
 func _on_join_pressed():
-	#peer.create_client("20.172.229.169",8080)
-	peer.create_client("localhost",8080)
+	peer.create_client("20.172.229.169",8080)
+	#peer.create_client("localhost",8080)
 	#peer.create_client("192.168.1.68",123)
 	multiplayer.multiplayer_peer = peer
-	#cam.clear_current()
 
 func add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
 	$PlayersMan.add_child(player)
-
-
 
 func exit_game(id):
 	multiplayer.peer_disconnected.connect(del_player)
@@ -38,5 +35,8 @@ func del_player(id):
 	rpc("_del_player", id)
 
 @rpc("any_peer", "call_local") func _del_player(id):
-	get_node(str(id)).queue_free()
+	$PlayersMan.get_node(str(id)).queue_free()
 
+
+func _on_quit_pressed():
+	exit_game(get_multiplayer_authority())
